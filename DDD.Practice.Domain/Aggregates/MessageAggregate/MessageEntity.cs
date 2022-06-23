67d741547762749908ab57eb83e991c8a1d29765
DOMAIN\Annie0318_Kuo <Annie0318_Kuo@pxmart.com.tw>
2022-06-23 13:44:35 +0800

    feat: create Domain and Message Aggregate

---
+++ DDD.Practice.Domain/Aggregates/MessageAggregate/MessageEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Practice.Domain.Aggregates.MessageAggregate
+{
+    internal class MessageEntity
+    {
+    }
+}
---
+++ DDD.Practice.Domain/SeedWork/IAggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Practice.Domain.SeedWork
+{
+    public interface IAggregateRoot
+    {
+        // 給 Aggregate 中的 MainEntity 繼承
+    }
+}
